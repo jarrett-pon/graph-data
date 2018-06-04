dfeb36e045a04ce570591b1d7b4ecb6a6c963ed3
Jarrett Pon <jpon@sfmbpjpon.local>
2018-06-04 16:35:19 -0700

    Add ability to export as react component

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ if (MINIFY) {
   }));
 }
 
-const config = {
+const defaultConfig = {
     entry: {
       'default': './src/builds/default.js'
     },
@@ -46,4 +46,25 @@ const config = {
     plugins: plugins
 };
 
-module.exports = config;
+const packagedConfig = {
+  entry: './src/app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
+  module : {
+      loaders : [
+          {
+              test : /\.jsx?/,
+              exclude: /node_modules/,
+              loader : 'babel-loader'
+          }
+      ]
+  },
+  externals: {
+    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  }
+}
+
+module.exports = [defaultConfig, packagedConfig];
